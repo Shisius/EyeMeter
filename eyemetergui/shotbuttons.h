5ac70e6e22fb7a12a19b49f7c92f332700c99739
Klenchik <123@gmail.com>
2025-05-01 17:03:14 +0300

    img

---
+++ eyemetergui/shotbuttons.h
@@ -0,0 +1,42 @@
+#ifndef SHOTBUTTONS_H
+#define SHOTBUTTONS_H
+
+#include <QWidget>
+#include <QPushButton>
+enum SHOT_BUTTON
+{
+    SHOT,
+    BACK, //
+    SOUND, //
+    LIGHTON,//
+    LIGHTOFF
+};
+class ShotButtons : public QWidget
+{
+    Q_OBJECT
+    static constexpr uint bigSide = 36;
+    static constexpr uint midSide = 24;
+    static constexpr uint offset = 20;
+
+    QPushButton *d_but_shot;
+    QPushButton *d_but_back;
+    QPushButton *d_but_sound;
+    QPushButton *d_but_light;
+
+    bool d_isResized=false;
+    bool d_isLightOn = false;
+public:
+    explicit ShotButtons(QWidget *parent);
+    virtual ~ShotButtons(){}
+    void resize(int w, int h);
+    void setLightOn(bool);
+signals:
+    void shotBut_clicked(uint);
+private slots:
+    void slot_shot();
+    void slot_back();
+    void slot_sound();
+    void slot_light();
+};
+
+#endif // SHOTBUTTONS_H
