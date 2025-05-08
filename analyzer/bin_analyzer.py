0f345e4b540d77897eb93318d7caac98d056e299
cogniscy_enigmae <totktoest@list.ru>
2025-05-09 00:36:11 +0300

    im sz 240

--- analyzer/bin_analyzer.py
+++ analyzer/bin_analyzer.py
@@ -191,8 +191,9 @@ class EyeAnalyzer:
                  load_model_path='.\\weights\\yolo_eye.pt',
                  rknn_model_path='.\\weights\\yolov8_seg.rknn',
                  rknn_model_path_320='.\\weights\\yolov8_seg_320.rknn',
+                 rknn_model_path_240='.\\weights\\yolov8_seg_240.rknn',
                  rknn_model_path_160='.\\weights\\yolov8_seg_160.rknn',
-                 verbose=False, reverse=1, conf=0.4, backend_type='rknn', pipeline_version='2'):
+                 verbose=False, reverse=1, conf=0.5, backend_type='rknn', pipeline_version='2'):
         if pipeline_version =='1':
             assert reverse==-1, 'Reverse was abnormal previously'
         self.pipeline_version = pipeline_version
@@ -204,6 +205,7 @@ class EyeAnalyzer:
             from rknn_pupil_detection import PupilDetectRKNN
             self.pd = PupilDetectRKNN(rknn_model=self.adj_os(rknn_model_path), conf=conf, iou=0.5, imgsz=640)
             self.pd_320 = PupilDetectRKNN(rknn_model=self.adj_os(rknn_model_path_320), conf=conf, iou=0.5, imgsz=320)
+            self.pd_240 = PupilDetectRKNN(rknn_model=self.adj_os(rknn_model_path_240), conf=conf, iou=0.5, imgsz=240)
             self.pd_160 = PupilDetectRKNN(rknn_model=self.adj_os(rknn_model_path_160), conf=conf, iou=0.5, imgsz=160)
         elif backend_type == 'onnx':
             from onnx_pupil_detection import PupilDetectONNX
@@ -461,10 +463,13 @@ class EyeAnalyzer:
             with torch.inference_mode():
                 if img_sz == 320:
                     result = self.pd_320.model.predict([img[:, :, None].repeat(3, axis=-1)],
-                                                   save=False, imgsz=320, conf=self.pd.conf)
+                                                       save=False, imgsz=320, conf=self.pd.conf)
+                elif img_sz == 240:
+                    result = self.pd_240.model.predict([img[:, :, None].repeat(3, axis=-1)],
+                                                       save=False, imgsz=240, conf=self.pd.conf)
                 elif img_sz == 160:
                     result = self.pd_160.model.predict([img[:, :, None].repeat(3, axis=-1)],
-                                                   save=False, imgsz=160, conf=self.pd.conf)
+                                                       save=False, imgsz=160, conf=self.pd.conf)
                 else:
                     result = self.pd.model.predict([img[:, :, None].repeat(3, axis=-1)],
                                                    save=False, imgsz=min(self.pd.imgsz, img_sz), conf=self.pd.conf)
