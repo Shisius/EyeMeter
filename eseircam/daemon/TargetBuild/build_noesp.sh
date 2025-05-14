ad00128a5784361575b39569b04f58b8e0c89bfe
shisius <neuroslave.rnr@gmail.com>
2025-05-15 00:02:46 +0300

    noesp mode

--- eseircam/daemon/TargetBuild/build_noesp.sh
+++ eseircam/daemon/TargetBuild/build_noesp.sh
@@ -9,7 +9,7 @@ cd tmp/
 # run cmake
 result=0
 echo "Running cmake"
-cmake -DBUILD_FOLDER="${PWD}/.." -DDEFINE_90HZ_MODE=OFF -DDEFINE_NOESP_MODE=OFF ../../ 
+cmake -DBUILD_FOLDER="${PWD}/.." -DDEFINE_90HZ_MODE=OFF -DDEFINE_NOESP_MODE=ON ../../ 
 echo "Building project"
 make -j12 && result=1
 if [ $result == 1 ]
--- led/wiring/gpio_tools.cpp
+++ led/wiring/gpio_tools.cpp
@@ -28,6 +28,7 @@ int SysPwm::get_sys_number()
 
 int SysPwm::setup()
 {
+#ifndef SYSPWM_DUMMY
 	if (get_sys_number() < 0) return -1;
 	std::string filename = "/sys/class/pwm/pwmchip" + std::to_string(d_sys_pwm_num) + "/pwm0/duty_cycle";
 	d_duty_fd = open(filename.c_str(), O_RDWR);
@@ -35,12 +36,14 @@ int SysPwm::setup()
 		printf("SysPwm: open file %s failed\n", filename.c_str());
 		return -1;
 	}
-
+#endif
 	return 0;
+
 }
 
 int SysPwm::set_duty(float duty)
 {
+#ifndef SYSPWM_DUMMY
 	if (duty < 0 || duty > 1.0) {
 		printf("SysPwm:: wrong duty %f\n", duty);
 		return -1;
@@ -52,5 +55,6 @@ int SysPwm::set_duty(float duty)
 		printf("SysPwm:: write failed with result = %ld\n", result);
 		return -1;
 	}
+#endif
 	return 0;
 }
